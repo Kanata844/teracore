6b9d2e35d120da2ae141fda57b9b2ac47f3181b5
kinako-pro <265059355+kinako-pro@users.noreply.github.com>
2026-03-13 12:42:07 +0900

    編集

--- src/routes/blog/03-Mastering/articles.ts
+++ src/routes/blog/03-Mastering/articles.ts
@@ -1,5 +1,14 @@
 import type { Article } from "$lib/data";
 
 export const articles: Article[] = [
-    //この配列を、記事のリストにする
+    {
+        id: '03-Mastering-01',
+        cat: '03-Mastering',
+        path: '/blog/03-Mastering/file5-1',
+        title: 'Ⅲ / 第０講「プロローグ」',
+        ath: '03',
+        date: '2026-03-13',
+        index: 1
+
+    }
 ];
--- src/routes/blog/03-StudioOne/articles.ts
+++ src/routes/blog/03-StudioOne/articles.ts
@@ -5,7 +5,7 @@ export const articles: Article[] = [
         id: '03-StudioOne-01',
         cat: '03-StudioOne',
         path: '/blog/03-StudioOne/file3-1',
-        title: 'Ⅰ / 第一講「音声ファイルとは」',
+        title: 'Ⅰ / 第０講「講義の前に」',
         ath: '03',
         date: '2026-03-05',
         index: 1,
